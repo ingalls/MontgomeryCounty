d8bd8fe81c4f09a9eefbd4303c387caf205d7137
ingalls <nick@mapbox.com>
2016-02-16 21:46:46 +0000

    better formatting

--- fishy.sh
+++ fishy.sh
@@ -2,7 +2,8 @@
 RESULTS=0
 LISTS=0
 
-echo "- Problematic Files"
+echo ""
+echo "Problematic Files"
 for FILE in results/*; do
     OK=0
 
@@ -10,9 +11,11 @@ for FILE in results/*; do
 
     if [[ $(grep -m1 -Eo "ucSearchResult" $FILE | wc -l) > 0 ]]; then OK=1 && LISTS=$(($LISTS+1)); fi
 
-    if [[ $OK != 1 ]]; then echo "  - $FILE"; fi
+    if [[ $OK != 1 ]]; then echo "- $FILE"; fi
 done
 
 echo ""
 echo "Good Results: $RESULTS" 
 echo "List Results: $LISTS" #Addresses with more than one account #
+echo "-------------------"
+echo "Total       : $(ls results/ | wc -l)"
