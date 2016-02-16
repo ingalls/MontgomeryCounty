d77e33f1485270207babd1570aa5e85447e398fd
ingalls <nick@mapbox.com>
2016-02-16 21:44:28 +0000

    Look for signs that a result failed

--- fishy.sh
+++ fishy.sh
@@ -1,1 +1,18 @@
-grep -L "Account Identifier" results/*
+
+RESULTS=0
+LISTS=0
+
+echo "- Problematic Files"
+for FILE in results/*; do
+    OK=0
+
+    if [[ $(grep -m1 -Eo "Account Identifier" $FILE | wc -l) > 0 ]]; then OK=1 && RESULTS=$(($RESULTS+1)); fi
+
+    if [[ $(grep -m1 -Eo "ucSearchResult" $FILE | wc -l) > 0 ]]; then OK=1 && LISTS=$(($LISTS+1)); fi
+
+    if [[ $OK != 1 ]]; then echo "  - $FILE"; fi
+done
+
+echo ""
+echo "Good Results: $RESULTS" 
+echo "List Results: $LISTS" #Addresses with more than one account #
